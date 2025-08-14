66c8150664f73a2b53bbedfef3494798c5f236a9
phoebe <leeee.phoebe@gmail.com>
2025-08-13 20:20:48 -0500

    created load_gz & save_gz functions; changed add function signature & added hashing

--- central.py
+++ central.py
@@ -1,4 +1,4 @@
-import time, os, json, gzip
+import time, os, json, gzip, hashlib
 import numpy as np
 from google import genai
 import config
@@ -8,56 +8,53 @@ e_data = None # embedding data
 m_data = None # meta data
 
 cos_sim = lambda v1, v2: np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
-
+def load_gz(path):
+    with gzip.open(path, 'rt') as f:
+        try:
+            data = json.load(f)
+        except:
+            data = {}
+    return data
+def save_gz(data, path):
+    with gzip.open(path, 'wt') as f:
+        json.dump(data, f)
 def init():
     global e_data, m_data
-    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, "embedding.gz"), 'rt') as f:
-        try:
-            e_data = json.load(f)
-        except:
-            e_data = {}
-    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, "meta.gz"), 'rt') as f:
-        try:
-            m_data = json.load(f)
-        except:
-            m_data = {}
+    e_data = load_gz(os.path.join(config.CENTRAL_DATA_PATH, "embedding.gz"))
+    m_data = load_gz(os.path.join(config.CENTRAL_DATA_PATH, "meta.gz"))
 init()
-
-def add(doc_id, source, doc_path=None):
-    score = 0
+def sha256str(s):
+    h = hashlib.sha256()
+    h.update(s.encode('utf-8'))
+    return h.hexdigest()
+def add(content, meta={}):
     doc_dir = os.path.join(config.CENTRAL_DATA_PATH, "documents")
-    if doc_path:
-        with open(doc_path, 'r') as f:
-            doc = f.read()
-    else:
-        doc_path = os.path.join(doc_dir, f"{doc_id}.txt")
-        doc = input("File contents: ")
-        with open(doc_path, 'w') as f:
-            f.write(doc)
+    doc_id = sha256str(content)
+    doc_path = os.path.join(doc_dir, f"{doc_id}.txt")
+    with open(doc_path, 'w') as f:
+        json.dump({"content": content}, f)
     doc_embed = client.models.embed_content(
         model="gemini-embedding-exp-03-07",
-        contents=doc,
+        contents=content,
         config={"task_type": "retrieval_document"})
-    m_entry = {"time": time.time(), "source": source, "score": score}
+    m_entry = {"time": time.time(),
+               "source": meta.get("source", "N/A"),
+               "score": meta.get("score", 0)}
     e_entry = [e.values for e in doc_embed.embeddings]
     global e_data, m_data
     e_data[f"{doc_id}"] = e_entry[0]
     m_data[f"{doc_id}"] = m_entry
-    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, "embedding.gz"), 'wt') as f:
-        json.dump(e_data, f, indent=4)
-    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, "meta.gz"), 'wt') as f:
-        json.dump(m_data, f, indent=4)
-    return
+    save_gz(e_data, os.path.join(config.CENTRAL_DATA_PATH, "embedding.gz"))
+    save_gz(m_data, os.path.join(config.CENTRAL_DATA_PATH, "meta.gz"))
+    return doc_id
 def delete(doc_id):
     global e_data, m_data
     doc_path = f"{config.CENTRAL_DATA_PATH}documents/{doc_id}.txt"
     os.remove(doc_path)
     del e_data[f"{doc_id}"]
-    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, "embedding.gz"), 'wt') as f:
-        json.dump(e_data, f, indent=4)
     del m_data[f"{doc_id}"]
-    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, "meta.gz"), 'wt') as f:
-        json.dump(m_data, f, indent=4)
+    save_gz(e_data, os.path.join(config.CENTRAL_DATA_PATH, "embedding.gz"))
+    save_gz(m_data, os.path.join(config.CENTRAL_DATA_PATH, "meta.gz"))
     return
 def search(q, n=5, threshold=0):
     result = []
