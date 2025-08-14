6c05ee93ac5015b416313528cb9b6513bcb7f8de
phoebe <97257736+leephoebe@users.noreply.github.com>
2025-08-14 10:11:13 -0500

    created load_gz & save_gz functions; changed add function signature &… (#1)
    Co-authored-by: Lab Web <30175757+phantomlsh@users.noreply.github.com>

--- central.py
+++ central.py
@@ -1,4 +1,4 @@
-import time, os, json, gzip
+import time, os, json, gzip, hashlib
 import numpy as np
 from google import genai
 import config
@@ -7,66 +7,59 @@ client = genai.Client(api_key=config.GEMINI_API_KEY)
 e_data = None # embedding data
 m_data = None # meta data
 
+# helper functions
 cos_sim = lambda v1, v2: np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+def load_gz(path):
+    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, path), 'rt') as f:
+        try:
+            data = json.load(f)
+        except:
+            data = {}
+    return data
+def save_gz(path, data):
+    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, path), 'wt') as f:
+        json.dump(data, f)
+def sha256str(s):
+    h = hashlib.sha256()
+    h.update(s.encode('utf-8'))
+    return h.hexdigest()
+def embed(content, task_type="retrieval_document"):
+    try:
+        return client.models.embed_content(model="gemini-embedding-exp-03-07", contents=content, config={"task_type": task_type}).embeddings[0].values
+    except:
+        return None
 
 def init():
     global e_data, m_data
-    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, "embedding.gz"), 'rt') as f:
-        try:
-            e_data = json.load(f)
-        except:
-            e_data = {}
-    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, "meta.gz"), 'rt') as f:
-        try:
-            m_data = json.load(f)
-        except:
-            m_data = {}
+    e_data = load_gz("embedding.gz")
+    m_data = load_gz("meta.gz")
 init()
 
-def add(doc_id, source, doc_path=None):
-    score = 0
-    doc_dir = os.path.join(config.CENTRAL_DATA_PATH, "documents")
-    if doc_path:
-        with open(doc_path, 'r') as f:
-            doc = f.read()
-    else:
-        doc_path = os.path.join(doc_dir, f"{doc_id}.txt")
-        doc = input("File contents: ")
-        with open(doc_path, 'w') as f:
-            f.write(doc)
-    doc_embed = client.models.embed_content(
-        model="gemini-embedding-exp-03-07",
-        contents=doc,
-        config={"task_type": "retrieval_document"})
-    m_entry = {"time": time.time(), "source": source, "score": score}
-    e_entry = [e.values for e in doc_embed.embeddings]
+# operations
+def add(content, meta={}):
     global e_data, m_data
-    e_data[f"{doc_id}"] = e_entry[0]
-    m_data[f"{doc_id}"] = m_entry
-    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, "embedding.gz"), 'wt') as f:
-        json.dump(e_data, f, indent=4)
-    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, "meta.gz"), 'wt') as f:
-        json.dump(m_data, f, indent=4)
-    return
-def delete(doc_id):
+    doc_id = sha256str(content)
+    save_gz(f"documents/{doc_id}.gz", {"content": content})
+    meta["time"] = int(time.time())
+    e_data[doc_id] = embed(content)
+    m_data[doc_id] = meta
+    save_gz("embedding.gz", e_data)
+    save_gz("meta.gz", m_data)
+    return doc_id
+def del(doc_id):
     global e_data, m_data
-    doc_path = f"{config.CENTRAL_DATA_PATH}documents/{doc_id}.txt"
+    doc_path = os.path.join(config.CENTRAL_DATA_PATH, f"documents/{doc_id}.gz")
     os.remove(doc_path)
-    del e_data[f"{doc_id}"]
-    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, "embedding.gz"), 'wt') as f:
-        json.dump(e_data, f, indent=4)
-    del m_data[f"{doc_id}"]
-    with gzip.open(os.path.join(config.CENTRAL_DATA_PATH, "meta.gz"), 'wt') as f:
-        json.dump(m_data, f, indent=4)
+    del e_data[doc_id]
+    del m_data[doc_id]
+    save_gz("embedding.gz", e_data)
+    save_gz("meta.gz", m_data)
     return
 def search(q, n=5, threshold=0):
     result = []
     global e_data
     score = 0
-    q_embed = client.models.embed_content(model="gemini-embedding-exp-03-07",
-                                        contents=q,
-                                        config={"task_type": "retrieval_query"})
-    q_embedding = np.array(q_embed.embeddings[0].values)
+    q_embedding = embed(q, task_type="retrieval_query")
     for doc_id, embedding in e_data.items():
         score = cos_sim(q_embedding, np.array(embedding))
         if score >= threshold:
