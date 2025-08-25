8a048c508ec9916fa9faf55d8d81cf53b6ec5623
phoebe <leeee.phoebe@gmail.com>
2025-08-25 09:32:35 -0500

    thinking off + structured output

--- ai.py
+++ ai.py
@@ -4,13 +4,22 @@ import time
 
 def check(client, query, docs):
     text = "-----\n".join(f"Document ID: {d['id']}: \n {d['content']}" for d in docs)
-    system_instruction = "Given the following text documents, determine if there is enough information to solve the problem. If not, recommend a specific keyword or query to search for the missing information."
+    system_instruction = """Given the following text documents, determine if there is enough information to solve the problem.
+    If there is, set 'sufficient' to true.
+    If not, set 'sufficient' to false, and recommend a specific keyword or query to search for the missing information in the 'search_query' field.
+    The output must be a JSON object with the following structure:
+    {
+        "sufficient": true | false
+        "search_query": "specific keyword or query to search"
+    }
+    """
     contents = f"Problem: {query}\n\nDocuments:\n{text}"
     try:
         res = client.models.generate_content(
             model="gemini-2.5-flash",
             config=types.GenerateContentConfig(
-            system_instruction=system_instruction),
+            system_instruction=system_instruction,
+            thinking_config=types.ThinkingConfig(thinking_budget=0)), # Turn off thinking
             contents=contents)
         return res.text
     except genai.types.APIError as e:
