c373366baa567bd7616339cb8aada11d1df5423e
phoebe <97257736+leephoebe@users.noreply.github.com>
2025-08-25 15:39:44 -0500

    Ai (#4)

--- ai.py
+++ ai.py
@@ -1,32 +1,74 @@
 from google import genai
 from google.genai import types
-from google.genai.types import ModelContent, Part, UserContent
 import time
 
-def gen(client, query, docs, history):
-    text = "\n".join(f"Document ID: {d['id']}: \n {d['content']}" for d in docs)
-    prompt = f"""You are an AI assistant and coding expert for an experimental quantum research lab.
-
-    Here is the question:
-    "{query}"
-
-    Here is the relevant information:
-    "{text}"
-
-    Here is the history:
-    "{history}
-
-    Using ONLY the relevant information and history, answer the question concisely with NO comments. When you use information from a document, cite its ID like this: 'Document(s) used: [ID]'.
+def check(client, query, docs):
+    text = "-----\n".join(f"Document ID: {d['id']}: \n {d['content']}" for d in docs)
+    system_instruction = """Given the following text documents, determine if there is enough information to solve the problem.
+    If not, recommend specific keyword(s) to search for the missing information in the 'keywords' field.
     """
-    history.append(UserContent(parts=[Part(text=query)]))
+    contents = f"Problem: {query}\n\nDocuments:\n{text}"
+    try:
+        res = client.models.generate_content(
+            model="gemini-2.5-flash",
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction,
+                thinking_config=types.ThinkingConfig(thinking_budget=0),
+                response_mime_type="application/json",
+                response_schema=genai.types.Schema(
+                    type = genai.types.Type.OBJECT,
+                    required = ["keywords"],
+                    properties = {
+                        "keywords": genai.types.Schema(
+                            type = genai.types.Type.ARRAY,
+                            items = genai.types.Schema(
+                                type = genai.types.Type.STRING,
+                            ),
+                        ),
+                    },
+                )
+            ),
+            contents=contents)
+        return res.text
+    except genai.types.APIError as e:
+        if e.status_code == 429:
+            print("resource exhaustion; retrying after delay")
+            time.sleep(5)
+            res = client.models.generate_content(
+            model="gemini-2.5-flash",
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction,
+                thinking_config=types.ThinkingConfig(thinking_budget=0),
+                response_mime_type="application/json",
+                response_schema=genai.types.Schema(
+                    type = genai.types.Type.OBJECT,
+                    required = ["keywords"],
+                    properties = {
+                        "keywords": genai.types.Schema(
+                            type = genai.types.Type.ARRAY,
+                            items = genai.types.Schema(
+                                type = genai.types.Type.STRING,
+                            ),
+                        ),
+                    },
+                )
+            ),
+            contents=contents)
+            return res.text
+        else:
+            raise
+
+def gen(client, query, docs):
+    text = "\n".join(f"Document ID: {d['id']}: \n {d['content']}" for d in docs)
+    system_instruction = "You are an AI assistant and coding expert for an experimental quantum research lab. When you use information from a document, cite its ID like this: 'Document(s) used: [ID]'. Answer the question concisely with NO comments and using ONLY the provided relevant text."
+    contents = f"Here is the question:\n\"{query}\"\n\nHere are the relevant documents, separated by a dashed line '-----':\n\"{text}\""
     try:
         res = client.models.generate_content(
             model="gemini-2.5-pro",
             config=types.GenerateContentConfig(
-            system_instruction=prompt),
-            contents=history)
-        history.append(ModelContent(parts=[Part(text=res.text)]))
-        return res.text, history
+            system_instruction=system_instruction),
+            contents=contents)
+        return res.text
     except genai.types.APIError as e:
         if e.status_code == 429:
             print("resource exhaustion; retrying after delay")
@@ -34,9 +76,8 @@ def gen(client, query, docs, history):
             res = client.models.generate_content(
                 model="gemini-2.5-pro",
                 config=types.GenerateContentConfig(
-                system_instruction=prompt),
-                contents=history)
-            history.append(ModelContent(parts=[Part(text=res.text)]))
-            return res.text, history
+                system_instruction=system_instruction),
+                contents=contents)
+            return res.text
         else:
             raise
