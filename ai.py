6cf3485eb2acd7ecddd94596408291391eab0959
phoebe <leeee.phoebe@gmail.com>
2025-08-26 14:07:17 -0500

    change arg type & return

--- ai.py
+++ ai.py
@@ -10,30 +10,33 @@ def gather_document(client, query, docs=[]):
         "parameters": {
             "type": "object",
             "properties": {
-                "keyword": {
-                    "type": "string"
-                },
+                "keywords": {
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    }
+                }
             },
-            "required": ["keyword"]
+            "required": ["keywords"]
         }
     }
     tools = types.Tool(function_declarations=[search_declaration])
-    def ai_search(keyword):
-        print("ai_search called")
+    def ai_search(keywords):
         docs_added = []
-        new_docs = search(keyword)
-        for d in new_docs:
-            if d in docs:
-                print("Document already presented: ", d['id'])
-            else:
-                docs.append(d)
-                docs_added.append(d['id'])
-        res = f"Added docs: {', '.join(docs_added)}" if docs_added else "no new docs added"
-        return {"Result": res}
+        for k in keywords:
+            new_docs = search(k)
+            for d in new_docs:
+                if d in docs:
+                    print("Document already presented: ", d['id'])
+                else:
+                    docs.append(d)
+                    docs_added.append(d['id'])
+        msg = f"Added docs: {', '.join(docs_added)}" if docs_added else "no new docs added"
+        return {"Result": msg}
     while True:
         text = "-----\n".join(f"Document ID: {d['id']}: \n {d['content']}" for d in docs)
         system_instruction = """Given the following text documents, determine if there is enough information to solve the problem.
-        If more information is needed, you MUST call the ai_search function with your recommended keyword.
+        If more information is needed, you MUST call the ai_search function with your recommended keyword(s).
         """
         config = types.GenerateContentConfig(
                     system_instruction=system_instruction,
@@ -59,13 +62,12 @@ def gather_document(client, query, docs=[]):
         tool_call = res.candidates[0].content.parts[0].function_call
         print("tool call: ", tool_call)
         if not tool_call:
-            print("complete search")
             break
         result = ai_search(**tool_call.args)
         f_res_part = types.Part.from_function_response(name=tool_call.name, response={"result": result})
         contents.append(res.candidates[0].content)
         contents.append(types.Content(role="user", parts=[f_res_part]))
-    return res.text
+    return docs
 
 def gen(client, query, docs):
     text = "\n".join(f"Document ID: {d['id']}: \n {d['content']}" for d in docs)
