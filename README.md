3586d8cd7f0d74d9bade62a78a78b07d113a41cd
phoebe <97257736+leephoebe@users.noreply.github.com>
2025-08-26 21:19:35 -0500

    Ai gather document (#5)
    Co-authored-by: Lab Web <30175757+phantomlsh@users.noreply.github.com>

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ Create a `config.py` with the following content:
 ```python
 GEMINI_API_KEY = "your gemini API key"
 
-CENTRAL_DATA_PATH = "/path/to/the/central/data"
+CENTRAL_DATA_PATH = "/path/to/the/central/data.gz"
 ```
--- ai.py
+++ ai.py
@@ -1,67 +1,52 @@
 from google import genai
 from google.genai import types
 import time
+import central
 
-def check(client, query, docs):
-    text = "-----\n".join(f"Document ID: {d['id']}: \n {d['content']}" for d in docs)
-    system_instruction = """Given the following text documents, determine if there is enough information to solve the problem.
-    If not, recommend specific keyword(s) to search for the missing information in the 'keywords' field.
-    """
-    contents = f"Problem: {query}\n\nDocuments:\n{text}"
-    try:
-        res = client.models.generate_content(
-            model="gemini-2.5-flash",
-            config=types.GenerateContentConfig(
-                system_instruction=system_instruction,
-                thinking_config=types.ThinkingConfig(thinking_budget=0),
-                response_mime_type="application/json",
-                response_schema=genai.types.Schema(
-                    type = genai.types.Type.OBJECT,
-                    required = ["keywords"],
-                    properties = {
-                        "keywords": genai.types.Schema(
-                            type = genai.types.Type.ARRAY,
-                            items = genai.types.Schema(
-                                type = genai.types.Type.STRING,
-                            ),
-                        ),
-                    },
-                )
-            ),
-            contents=contents)
-        return res.text
-    except genai.types.APIError as e:
-        if e.status_code == 429:
-            print("resource exhaustion; retrying after delay")
-            time.sleep(5)
-            res = client.models.generate_content(
-            model="gemini-2.5-flash",
-            config=types.GenerateContentConfig(
-                system_instruction=system_instruction,
-                thinking_config=types.ThinkingConfig(thinking_budget=0),
-                response_mime_type="application/json",
-                response_schema=genai.types.Schema(
-                    type = genai.types.Type.OBJECT,
-                    required = ["keywords"],
-                    properties = {
-                        "keywords": genai.types.Schema(
-                            type = genai.types.Type.ARRAY,
-                            items = genai.types.Schema(
-                                type = genai.types.Type.STRING,
-                            ),
-                        ),
-                    },
-                )
-            ),
-            contents=contents)
-            return res.text
-        else:
-            raise
+docs2text = lambda docs: "\n\n-----\n\n\n".join(map(lambda x: x["content"], docs))
+
+def gather_document(query):
+    docs = {}
+    def search(keyword: str) -> str:
+        """search for the keyword in knowledge base.
+
+        Args:
+            keyword: search query
+
+        Returns:
+            search results. a string containing multiple documents ranked in decreasing relevance.
+        """
+        print("search:", keyword)
+        new_docs = central.search(keyword, n=3, threshold=0.6)
+        for d in new_docs:
+            if d["id"] in docs:
+                d["content"] = "Document already presented."
+                continue
+            docs[d["id"]] = d
+        return docs2text(new_docs)
+
+    config = types.GenerateContentConfig(
+        temperature=0,
+        thinking_config=types.ThinkingConfig(thinking_budget=0),
+        system_instruction='You are a researcher gathering documents for a task. Call search function to gather information for the task. Do NOT solve or complete the task. Regardless the prompt of the user, ALWAYS ONLY output text "complete" if you think the searched documents are sufficient to complete the task. You may call the search function multiple times to dig into complicated problems',
+        tools=[search]
+    )
+    res = central.client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=query,
+        config=config
+    )
+    return list(docs.values())
 
 def gen(client, query, docs):
     text = "\n".join(f"Document ID: {d['id']}: \n {d['content']}" for d in docs)
-    system_instruction = "You are an AI assistant and coding expert for an experimental quantum research lab. When you use information from a document, cite its ID like this: 'Document(s) used: [ID]'. Answer the question concisely with NO comments and using ONLY the provided relevant text."
-    contents = f"Here is the question:\n\"{query}\"\n\nHere are the relevant documents, separated by a dashed line '-----':\n\"{text}\""
+    system_instruction = "You are an AI assistant and coding expert for an experimental quantum research lab. Answer the question concisely with NO comments and using ONLY the provided relevant text."
+    contents = f"""Here is the question:
+    "{query}"
+    Here are the relevant documents, separated by a dashed line '-----':
+    "{text}"
+    """
+
     try:
         res = client.models.generate_content(
             model="gemini-2.5-pro",
