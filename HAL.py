fc028bf49e6494060f288887f290282d7d770be0
Lab Web <30175757+phantomlsh@users.noreply.github.com>
2025-08-29 08:42:05 -0500

    Add main entry

--- HAL.py
+++ HAL.py
@@ -1,6 +1,8 @@
 import sys
 
-def main():
-    print("open the pod bay doors")
+def main(query):
+    if "open the pod bay doors" in query.casefold():
+        return "I'm sorry, Dave. I'm afraid I can't do that."
+    # TODO: entry to AI is here.
 
 sys.modules[__name__] = main
