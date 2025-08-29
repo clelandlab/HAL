20a390b206ab723ae9a02ba1035cd4d7a7640f79
Lab Web <30175757+phantomlsh@users.noreply.github.com>
2025-08-29 08:44:16 -0500

    Update HAL

--- HAL.py
+++ HAL.py
@@ -2,7 +2,8 @@ import sys
 
 def main(query):
     if "open the pod bay doors" in query.casefold():
-        return "I'm sorry, Dave. I'm afraid I can't do that."
+        print("I'm sorry, Dave. I'm afraid I can't do that.")
+        return
     # TODO: entry to AI is here.
 
 sys.modules[__name__] = main
