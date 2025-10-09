88cbd3d03847ff614ce46ecdb5e0053bb2b709c6
Lab Web <30175757+phantomlsh@users.noreply.github.com>
2025-10-09 10:51:13 -0500

    Add STATE to code

--- HAL_code.py
+++ HAL_code.py
@@ -8,10 +8,10 @@ from utils import client, add_generative_cost, docs2text, evalStr
 
 get_exec_import = lambda var: evalStr(config.EXEC_IMPORT, var)
 
-def code(query, import_variable={ "name": "" }, silent=False):
+def code(query, import_variable={ "name": "HAL" }, silent=False):
     docs = gather_document(query, silent=silent)
     text = docs2text(docs)
-    system_instruction = f"You are a world class programming AI that generates Python code based on user requirements. Write the code using ONLY the following documents:\n\n\n{text}\n\n\nThe code should be self-contained and runnable. Do NOT include any side behaviors like printing messages. Absolutely do NOT include any comments or explanations. The following packages are already imported (do NOT import them again!):\n{get_exec_import(import_variable)}"
+    system_instruction = f"You are a world class programming AI that generates Python code based on user requirements. Write the code using ONLY the following documents:\n\n\n{text}\n\n\nThe code should be self-contained and runnable. Do NOT include any side behaviors like printing messages. Absolutely do NOT include any comments or explanations.\nTo export data or variables for later use, save them to the global dictionary `STATE`. The following packages are already imported (do NOT import them again!):\n{get_exec_import(import_variable)}"
     if not silent:
         print("[HAL] Coding...")
     res = client.models.generate_content(
@@ -27,7 +27,9 @@ def code(query, import_variable={ "name": "" }, silent=False):
     add_generative_cost(res)
     return json.loads(res.text)["code"]
 
-def _exec(code):
-    loc = {}
-    exec(code, globals(), loc)
-    return loc
+def _exec(code, STATE={}, import_variable={ "name": "HAL" }):
+    _code = get_exec_import(import_variable) + "\n\n" + code
+    try:
+        exec(_code, { "STATE": STATE }, { "STATE": STATE })
+    except Exception as e:
+        return str(e)
