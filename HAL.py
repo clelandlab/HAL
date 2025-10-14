f418dd4ab5204746160f41ec4ea78a87257ea0bf
Lab Web <30175757+phantomlsh@users.noreply.github.com>
2025-10-14 13:25:48 -0500

    Add HAL.code temporarily

--- HAL.py
+++ HAL.py
@@ -1,7 +1,9 @@
 import sys
+import ipywidgets as widgets
 from IPython.display import display, Markdown
 import memory
 from HAL_output import output
+from HAL_code import code, _exec
 
 _show = lambda x: display(Markdown("---\n\n" + x + "\n\n---\n\n"))
 
@@ -17,6 +19,7 @@ def HAL(query, t=""):
 
 HAL.memory = memory
 HAL.silent = False
+HAL.auto_exec = False
 
 def _search(*args, **kwargs):
     res = memory.search(*args, **kwargs)
@@ -33,3 +36,28 @@ def _search(*args, **kwargs):
 HAL.search = _search
 
 sys.modules[__name__] = HAL
+
+# Temporary functions:
+def _code(query, STATE={}):
+    c = code(query)
+    display(Markdown(f"---\n\n```python\n{c}\n```\n\n---"))
+    output = widgets.Output()
+    executed = False
+    def trigger_exec(b):
+        nonlocal executed
+        if executed:
+            return
+        executed = True
+        with output:
+            err = _exec(c, STATE)
+            if err is not None:
+                print("Execution Error: ", err)
+                return
+    button = widgets.Button(description="Auto Executed" if HAL.auto_exec else "Execute", button_style='' if HAL.auto_exec else 'primary')
+    button.on_click(trigger_exec)
+    display(button, output)
+    if HAL.auto_exec:
+        trigger_exec(None)
+
+HAL.code = _code
+
